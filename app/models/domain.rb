3a9d672d33020a2c41409b9016b2ad13c8e623a1
Kyle <kdornblaser@gmail.com>
2015-05-29 16:13:45 -0400

    Add validations to domain: unique name, requires name and parses the name into name.suffix.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,21 @@
 class Domain < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validate :valid_domain_name
+  before_validation :clean_name
+
+  private
+
+  def valid_domain_name
+    url = Domainatrix.parse(name)
+    if url.domain.blank? || url.public_suffix.blank?
+      errors.add(:name, 'must be valid domain name')
+    end
+  end
+
+  def clean_name
+    if name
+      url = Domainatrix.parse(name)
+      self.name = "#{url.domain}.#{url.public_suffix}"
+    end
+  end
 end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Domain, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a name' do
+    expect { Domain.create }.to_not change { Domain.count }
+  end
+
+  it 'has a unique name' do
+    Domain.create(name: 'google.com')
+    expect { Domain.create(name: 'google.com') }.to_not change { Domain.count }
+  end
+
+  it 'has one record, not matter the subdomain or path' do
+    Domain.create(name: 'google.com')
+    domains = %w(http://google.com www.google.com http://www.google.com
+                 https://www.google.com google.com/path/path/path/)
+    domains.each do |domain|
+      expect { Domain.create(name: domain) }.to_not change { Domain.count }
+    end
+  end
+
+  # 'anything.anything' is a valid domain name
+  it 'must be a valid domain name' do
+    domains = %w(google .com)
+    domains.each do |domain|
+      expect { Domain.create(name: domain) }.to_not change { Domain.count }
+    end
+  end
 end
