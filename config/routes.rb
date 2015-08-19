7d93186f5ea30b49c89019404f5c427967f223cd
keyl10 <kdornblaser@gmail.com>
2015-08-19 19:32:27 +0000

    Fix compare ids not working for domain names.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   # id and format specified to fix problem with using domain names as slugs
   resources :domains, only: [:index, :show, :create], id: /([^\/])+?/, format: /json|csv|xml|yaml/
-  get '/compare/:id1/:id2', to: 'domains#compare', as: 'compare'
+  get '/compare/:id1/:id2', format: /json|csv|xml|yaml/, to: 'domains#compare', as: 'compare', id1: /([^\/])+?/, id2: /([^\/])+?/
 end
